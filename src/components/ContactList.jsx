0d383f17b64a5961f8b0c8a4ec35e2c50438edc8
Charlosus <145377783+Charlosus@users.noreply.github.com>
2025-06-18 12:26:31 +0200

    5

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,6 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useState } from 'react';
 import { deleteContact } from '../redux/contactsOperations';
+import { ClipLoader } from 'react-spinners';
 
 export const ContactList = () => {
   const { items, isLoading, error } = useSelector(state => state.contacts);
@@ -28,7 +29,24 @@ export const ContactList = () => {
         />
       </label>
       <ul>
-        {isLoading && <p>Loading...</p>}
+        {isLoading && (
+          <div
+            style={{
+              position: 'fixed',
+              top: 0,
+              left: 0,
+              width: '100vw',
+              height: '100vh',
+              backgroundColor: 'rgba(255, 255, 255, 0.7)',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              zIndex: 9999,
+            }}
+          >
+            <ClipLoader color="#ff6b08" size={40} />
+          </div>
+        )}
         {error && <p>Error: {error}</p>}
         {filteredContacts.map(item => (
           <li key={item.id}>
--- src/redux/contactsOperations.js
+++ src/redux/contactsOperations.js
@@ -16,7 +16,7 @@ export const fetchContacts = createAsyncThunk(
 );
 
 export const addContact = createAsyncThunk(
-    'contacts/addContact',
+    'contacts/addCont',
     async({ name, number }, thunkAPI) => {
         try {
             const response = await axios.post('contacts', {

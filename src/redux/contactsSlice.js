289c9e6d172582ad193e9dae5fb6f29e97ec6560
Charlosus <145377783+Charlosus@users.noreply.github.com>
2025-06-18 10:19:34 +0200

    added fetch

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,28 +1,32 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { fetchContacts } from './contactsOperations';
 
+
+const handlePending = state => {
+    state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+    state.isLoading = false;
+    state.error = action.payload
+}
 const contactsSlice = createSlice({
     name: 'contacts',
-    initialState: [],
-    reducers: {
-        addContact: {
-            reducer(state, action) {
-                state.push(action.payload);
-            },
-            prepare(name, number) {
-                return {
-                    payload: {
-                        id: nanoid(),
-                        name,
-                        number,
-                    },
-                };
-            },
-        },
-        deleteContact(state, action) {
-            return state.filter(contact => contact.id !== action.payload);
-        },
+    initialState: {
+        items: [],
+        isLoading: false,
+        error: null
     },
+    extraReducers: builder => {
+        builder 
+        .addCase(fetchContacts.pending, handlePending)
+        .addCase(fetchContacts.fulfilled, (state, action) => {
+            state.isLoading =false;
+            state.error = null;
+            state.items = action.payload
+        })
+        .addCase(fetchContacts.rejected, handleRejected)
+    }
+ 
 });
 
-export const {addContact, deleteContact} = contactsSlice.actions;
-export const contactsReducer = contactsSlice.reducer;
